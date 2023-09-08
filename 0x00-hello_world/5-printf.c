c369ac68d3ee5e6503cc23c998f0594263308325
pfarelo nelwamondo <pfare5057@gmail.com>
2023-09-07 18:28:22 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints a string of characters using the printf function
+ *
+ * Return: returns zero after executing
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,");
+	return (0);
+}
