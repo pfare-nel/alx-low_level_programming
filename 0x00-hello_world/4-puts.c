c67d4eb59bb044bbc93847f3800698faf6d4870f
pfarelo nelwamondo <pfare5057@gmail.com>
2023-09-07 18:17:48 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints a string of characters
+ *
+ * Return: returns zero
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
