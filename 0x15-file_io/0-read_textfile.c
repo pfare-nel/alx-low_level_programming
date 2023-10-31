44b93e42875581dedc899bb35b873c2726ef7991
pfarelo nelwamondo <pfare5057@gmail.com>
2023-10-30 19:56:21 -0700

    Adding a function that reads a text file and prints the letters

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,28 +9,65 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file_d;
-	ssize_t file_read, file_write;
-	char *buffer;
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
 	if (!filename)
 		return (0);
 
-	file_d = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 
-	if (file_d == -1)
+	if (fd == -1)
 		return (0);
 
-	buffer = malloc(sizeof(char) * (letters));
-	if (!buffer)
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
 		return (0);
 
-	file_read = read(file_d, buffer, letters);
-	file_write = write(STDOUT_FILENO, buffer, file_read);
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
-	close(file_d);
+	close(fd);
 
-	free(buffer);
+	free(buf);
 
-	return (file_read);
+	return (nwr);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
